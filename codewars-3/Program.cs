4be3f8b6e1d443d12382f758ec37cc33fdff05f5
HornyFerret <ombro.de.morto@gmail.com>
2021-07-10 15:57:45 +0300

    Добавьте файлы проекта.

---
+++ codewars-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace codewars_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[] { 1, 2, 1, 2 };
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(x[i]);
+            }
+            Console.WriteLine("------");
+            Maps(x);
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(x[i]);
+            }
+        }
+        public static int[] Maps(int[] x)
+        {
+
+            for (int i = 0;i<x.Length;i++)
+            {
+                x[i] = x[i] + x[i];
+            }
+            return x;
+        }
+    }
+}
